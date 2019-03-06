0e5c1646ed57c255b5faaa078b0f800dc1ac1039
Michael Yankura <myankura210@gmail.com>
2019-03-06 13:56:13 -0600

    completed practice exercise

---
+++ src/scripts/mirror.js
@@ -0,0 +1,32 @@
+console.log("mirror.js")
+
+//declare variable to grab input element
+const messageEl = document.getElementById("message");
+const outputEl = document.getElementById("article1");
+const outputEl2 = document.getElementById("article2");
+
+//Write an event listener that listens for the keyup event on the input field.
+
+messageEl.addEventListener("keyup", function(e) {
+    outputEl.innerHTML = e.target.value;
+    outputEl2.innerHTML = e.target.value;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fieldEl.addEventListener("keyup", function (event) {
+//     outputEl.innerHTML = event.target.value
+// })
